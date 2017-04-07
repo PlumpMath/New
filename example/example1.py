72a2ff3437d13dfe9c75091c5aff4a997931ca50
Vishakha-W <vishakhawaghmare10@gmail.com>
2017-04-08 00:45:14 +0530

    Add files via upload
    written coroutine(using generators)..

--- example/example1.py
+++ example/example1.py
@@ -8,6 +8,8 @@ from yapsy.PluginManager import PluginManager
 import logging
 logging.basicConfig(level=logging.DEBUG)
 from yapsy.IPlugin import IPlugin
+from plugins.plugin1 import PluginOne
+
 
 es = elasticsearch.Elasticsearch() # by default it takes 9200
 print(es.cat.health())
@@ -26,25 +28,28 @@ body = {
 
 
 def main():   
-    global p
+    global p1
     #  Load the plugins from the plugin directory.
     manager = PluginManager()
     manager.setPluginPlaces(["plugins"])
     manager.collectPlugins()
     # Loop round the plugins and print their names.
     for plugin in manager.getAllPlugins():
-        p=plugin.plugin_object.print_name()
-        print("path is:",p)
-        plugin.plugin_object.convert_from_xml(p)  
-        #plugin.plugin_object.convert_from_xlsx()
+        print("==========>  ",format(plugin.plugin_object))
+        #p="C:\Python34\directory1"
+        #print("path is:======>",p)
+        p1=plugin.plugin_object.print_name()
+        print(p1) 
+        p2=PluginOne()
+        p2.g()
+    
         
-
-   
 if __name__ == "__main__":
     main()
 
-glob.glob(p)
-os.chdir(p) 
+
+glob.glob(p1)
+os.chdir(p1) 
 for file in glob.glob("*.txt"):
         with open(file, 'r') as f:
         	data = base64.b64encode(bytes(f.read(),'utf-8')).decode('ascii');
